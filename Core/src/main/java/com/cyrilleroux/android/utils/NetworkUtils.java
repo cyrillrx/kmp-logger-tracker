25f2e418006a6053d7d6caafb1ffbd5e71c42d79
Cyril Leroux <cyril0leroux@gmail.com>
2015-03-01 23:26:57 +0100

    Created NetworkUtils class

---
+++ Core/src/main/java/com/cyrilleroux/android/utils/NetworkUtils.java
@@ -0,0 +1,18 @@
+package com.cyrilleroux.android.utils;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+/**
+ * @author Cyril Leroux
+ *         Created 01/03/2015.
+ */
+public class NetworkUtils {
+
+    public static boolean isConnected(Context context) {
+        final ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        final NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        return netInfo != null && netInfo.isConnected();
+    }
+}
