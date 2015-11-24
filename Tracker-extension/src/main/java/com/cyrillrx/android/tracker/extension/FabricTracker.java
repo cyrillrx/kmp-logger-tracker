5d7367c8e8d63264b82b0812621ff973b46f493a
Cyril Leroux <cyril0leroux@gmail.com>
2015-11-25 00:13:02 +0100

    Implemented basic FabricTracker

--- Tracker-extension/src/main/java/com/cyrillrx/android/tracker/extension/FabricTracker.java
+++ Tracker-extension/src/main/java/com/cyrillrx/android/tracker/extension/FabricTracker.java
@@ -1,10 +1,15 @@
 package com.cyrillrx.android.tracker.extension;
 
-import android.support.annotation.NonNull;
+import android.content.Context;
 
+import com.crashlytics.android.answers.Answers;
+import com.crashlytics.android.answers.CustomEvent;
+import com.crashlytics.android.answers.RatingEvent;
 import com.cyrillrx.android.tracker.TrackFilter;
 import com.cyrillrx.android.tracker.TrackWrapper;
 import com.cyrillrx.android.tracker.TrackerChild;
+import com.cyrillrx.android.tracker.event.EntityEvent;
+import com.cyrillrx.android.tracker.event.TrackEvent;
 
 /**
  * @author Cyril Leroux
@@ -12,7 +17,31 @@ import com.cyrillrx.android.tracker.TrackerChild;
  */
 public class FabricTracker extends TrackWrapper {
 
-    public FabricTracker(@NonNull TrackerChild tracker, TrackFilter filter) {
-        super(tracker, filter);
+    public FabricTracker(TrackFilter filter) {
+        super(new FabricTrackChild(), filter);
+    }
+
+    private static class FabricTrackChild implements TrackerChild {
+
+        @Override
+        public void track(Context context, TrackEvent event) {
+            if (event instanceof EntityEvent) {
+                trackRating((EntityEvent) event);
+            } else {
+                track(event);
+            }
+        }
+
+        private void track(TrackEvent event) {
+            Answers.getInstance().logCustom(new CustomEvent(event.getName()));
+        }
+
+        private void trackRating(EntityEvent event) {
+            Answers.getInstance().logRating(new RatingEvent()
+                    .putRating(event.getIntValue())
+                    .putContentName(event.getName())
+                    .putContentType(event.getType())
+                    .putContentId(event.getId()));
+        }
     }
 }
