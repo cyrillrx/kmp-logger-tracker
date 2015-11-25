d41c88d045e1420afd6707bac1c1197ed244b2d9
Cyril Leroux <cleroux@molotov.tv>
2015-11-25 08:31:24 +0100

    2015-11-25 - Tracker-extension v0.1.0 - Added implementation for Answer (Fabric) and Google Analytics trackers

--- Tracker-extension/src/main/java/com/cyrillrx/android/tracker/extension/AnalyticsTracker.java
+++ Tracker-extension/src/main/java/com/cyrillrx/android/tracker/extension/AnalyticsTracker.java
@@ -1,18 +1,32 @@
 package com.cyrillrx.android.tracker.extension;
 
-import android.support.annotation.NonNull;
-
 import com.cyrillrx.android.tracker.TrackFilter;
 import com.cyrillrx.android.tracker.TrackWrapper;
 import com.cyrillrx.android.tracker.TrackerChild;
+import com.cyrillrx.android.tracker.TrackerContext;
+import com.cyrillrx.android.tracker.event.TrackEvent;
 
 /**
+ * A {@link TrackWrapper} wrapping a Google Analytics {@link TrackerChild}.
+ *
  * @author Cyril Leroux
  *         Created on 11/11/2015.
  */
 public class AnalyticsTracker extends TrackWrapper {
 
-    public AnalyticsTracker(TrackFilter filter, @NonNull TrackerChild tracker) {
-        super(tracker, filter);
+    public AnalyticsTracker(TrackFilter filter) {
+        super(new AnalyticsTrackChild(), filter);
+    }
+
+    public AnalyticsTracker() {
+        super(new AnalyticsTrackChild());
+    }
+
+    private static class AnalyticsTrackChild implements TrackerChild {
+
+        @Override
+        public void track(TrackerContext context, TrackEvent event) {
+            // TODO
+        }
     }
 }
--- Tracker-extension/src/main/java/com/cyrillrx/android/tracker/extension/FabricTracker.java
+++ Tracker-extension/src/main/java/com/cyrillrx/android/tracker/extension/FabricTracker.java
@@ -1,17 +1,18 @@
 package com.cyrillrx.android.tracker.extension;
 
-import android.content.Context;
-
 import com.crashlytics.android.answers.Answers;
 import com.crashlytics.android.answers.CustomEvent;
 import com.crashlytics.android.answers.RatingEvent;
 import com.cyrillrx.android.tracker.TrackFilter;
 import com.cyrillrx.android.tracker.TrackWrapper;
 import com.cyrillrx.android.tracker.TrackerChild;
+import com.cyrillrx.android.tracker.TrackerContext;
 import com.cyrillrx.android.tracker.event.EntityEvent;
 import com.cyrillrx.android.tracker.event.TrackEvent;
 
 /**
+ * A {@link TrackWrapper} wrapping a Answer (Fabric) {@link TrackerChild}.
+ *
  * @author Cyril Leroux
  *         Created on 11/11/2015.
  */
@@ -21,27 +22,43 @@ public class FabricTracker extends TrackWrapper {
         super(new FabricTrackChild(), filter);
     }
 
+    public FabricTracker() {
+        super(new FabricTrackChild());
+    }
+
     private static class FabricTrackChild implements TrackerChild {
 
         @Override
-        public void track(Context context, TrackEvent event) {
+        public void track(TrackerContext context, TrackEvent event) {
+
             if (event instanceof EntityEvent) {
                 trackRating((EntityEvent) event);
+
             } else {
-                track(event);
+                trackCustom(event);
             }
         }
 
-        private void track(TrackEvent event) {
-            Answers.getInstance().logCustom(new CustomEvent(event.getName()));
+        private void trackRating(EntityEvent event) {
+            Answers.getInstance().logRating(
+                    new RatingEvent()
+                            .putRating(event.getIntValue())
+                            .putContentName(event.getName())
+                            .putContentType(event.getType())
+                            .putContentId(event.getId())
+                            .putCustomAttribute("createdAt", event.getCreatedAt())
+            );
         }
 
-        private void trackRating(EntityEvent event) {
-            Answers.getInstance().logRating(new RatingEvent()
-                    .putRating(event.getIntValue())
-                    .putContentName(event.getName())
-                    .putContentType(event.getType())
-                    .putContentId(event.getId()));
+        private void trackCustom(TrackEvent event) {
+            Answers.getInstance().logCustom(
+                    new CustomEvent(event.getName())
+                            .putCustomAttribute("category", event.getCategory())
+                            .putCustomAttribute("id", event.getId())
+                            .putCustomAttribute("type", event.getType())
+                            .putCustomAttribute("name", event.getType())
+                            .putCustomAttribute("createdAt", event.getCreatedAt())
+            );
         }
     }
 }
