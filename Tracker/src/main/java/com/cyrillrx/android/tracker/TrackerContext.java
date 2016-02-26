260d8e24c702415ea0e9e3f3bb9fef52dc5a3b8e
Cyril Leroux <cleroux@molotov.tv>
2016-02-26 10:21:28 +0100

    2016-02-26 - v0.4.7 - Added getter methods to TrackerContext class

--- Tracker/src/main/java/com/cyrillrx/android/tracker/TrackerContext.java
+++ Tracker/src/main/java/com/cyrillrx/android/tracker/TrackerContext.java
@@ -15,19 +15,19 @@ public class TrackerContext {
         return this;
     }
 
+    public App getApp() { return app; }
+
     public TrackerContext setUser(User user) {
         this.user = user;
         return this;
     }
 
+    public User getUser() { return user; }
+
     public TrackerContext setDevice(Device device) {
         this.device = device;
         return this;
     }
-
-    public App getApp() { return app; }
-
-    public User getUser() { return user; }
 
     public Device getDevice() { return device; }
 
@@ -43,6 +43,10 @@ public class TrackerContext {
             this.name = name;
             this.version = version;
         }
+
+        public String getName() { return name; }
+
+        public String getVersion() { return version; }
     }
 
     public static class User {
@@ -56,15 +60,21 @@ public class TrackerContext {
             return this;
         }
 
+        public String getId() { return id; }
+
         public User setName(String name) {
             this.name = name;
             return this;
         }
 
+        public String getName() { return name; }
+
         public User setEmail(String email) {
             this.email = email;
             return this;
         }
+
+        public String getEmail() { return email; }
     }
 
     public static class Device {
@@ -82,30 +92,42 @@ public class TrackerContext {
             return this;
         }
 
+        public String getOs() { return os; }
+
         public Device setBrand(String brand) {
             this.brand = brand;
             return this;
         }
+
+        public String getBrand() { return brand; }
 
         public Device setManufacturer(String manufacturer) {
             this.manufacturer = manufacturer;
             return this;
         }
 
+        public String getManufacturer() { return manufacturer; }
+
         public Device setModel(String model) {
             this.model = model;
             return this;
         }
+
+        public String getModel() { return model; }
 
         public Device setSerial(String serial) {
             this.serial = serial;
             return this;
         }
 
+        public String getSerial() { return serial; }
+
         public Device setDisplay(String display) {
             this.display = display;
             return this;
         }
+
+        public String getDisplay() { return display; }
     }
 
 }
