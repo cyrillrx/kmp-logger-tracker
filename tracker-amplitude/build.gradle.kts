7c4cf1b59b35e96e91141fa9d7ae85efc872501d
Cyril Leroux <cyril0leroux@gmail.com>
2021-08-06 13:38:46 +0200

    [Deps] Migrated :logger-logcat, :notifier and :tracker-amplitude to Gradle Kotlin DSL

---
+++ tracker-amplitude/build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    id("com.android.library")
+    kotlin("android")
+}
+
+android {
+    compileSdkVersion(Version.compileSdk)
+
+    defaultConfig {
+        minSdkVersion(Version.minSdk)
+        targetSdkVersion(Version.targetSdk)
+    }
+
+    lintOptions {
+        lintConfig = file("../lint.xml")
+    }
+}
+
+dependencies {
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    implementation(project(":logger"))
+    implementation(project(":tracker"))
+    implementation(project(":device"))
+
+    implementation("com.amplitude:android-sdk:2.16.0")
+
+    testImplementation("junit:junit:4.13.2")
+    testImplementation ("org.mockito:mockito-core:2.28.2")
+
+    androidTestImplementation ("androidx.annotation:annotation:1.2.0")
+    androidTestImplementation ("androidx.test:runner:1.4.0")
+    androidTestImplementation ("androidx.test:rules:1.4.0")
+}
