799137e2163ed0bca8251bbfc383570d5dc4dbe3
Cyril Leroux <cyril0leroux@gmail.com>
2021-08-06 13:36:20 +0200

    [Deps] Migrated :device's build.gradle to Gradle Kotlin DSL

---
+++ device/build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    id("com.android.library")
+    kotlin("android")
+}
+
+android {
+    compileSdkVersion(Version.compileSdk)
+    buildToolsVersion(Version.buildToolsVersion)
+
+    defaultConfig {
+        minSdkVersion(Version.minSdk)
+        targetSdkVersion(Version.targetSdk)
+
+        testInstrumentationRunner("androidx.test.runner.AndroidJUnitRunner")
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+}
+
+dependencies {
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    implementation("androidx.appcompat:appcompat:1.3.0")
+    implementation("androidx.core:core-ktx:1.6.0")
+
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test:runner:1.4.0")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+}
