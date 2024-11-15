3168d68608be5f49df1303357621c52204998c2e
Cyril Leroux <cyril0leroux@gmail.com>
2024-11-16 00:28:19 +0100

    Migrate logger:crashlytics dependencies to toml

--- logger/crashlytics/build.gradle.kts
+++ logger/crashlytics/build.gradle.kts
@@ -23,14 +23,6 @@ android {
 
 dependencies {
     implementation(projects.logger.lib)
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
-    implementation("androidx.annotation:annotation:1.3.0")
-    implementation("com.google.firebase:firebase-crashlytics:18.2.7")
-
-    testImplementation("junit:junit:4.13.2")
-    testImplementation("org.mockito:mockito-core:3.7.7")
-
-    androidTestImplementation("androidx.test:runner:1.4.0")
-    androidTestImplementation("androidx.test:rules:1.4.0")
+    implementation(libs.firebase.crashlytics)
 }
