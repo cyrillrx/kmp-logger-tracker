b6e223be5ca40f5d3be36a518baa5db1cc99aad5
Cyril Leroux <cyril0leroux@gmail.com>
2024-11-17 02:29:34 +0100

    Logger: Add emoji to Severity

--- logger/lib/src/commonMain/kotlin/com/cyrillrx/logger/Severity.kt
+++ logger/lib/src/commonMain/kotlin/com/cyrillrx/logger/Severity.kt
@@ -1,19 +1,20 @@
 package com.cyrillrx.logger;
 
 import kotlin.experimental.ExperimentalObjCName
+import kotlin.native.ObjCName
 
 /**
  * @author Cyril Leroux
  *         Created on 20/10/2015.
  */
-enum class Severity(val level: Int, val label: String) {
-    FATAL(0, "FATAL"),
-    ERROR(1, "ERROR"),
-    WARN(2, "WARN"),
-    INFO(3, "INFO"),
+enum class Severity(val level: Int, val label: String, val emoji: String) {
+    FATAL(0, "FATAL", "💥"),
+    ERROR(1, "ERROR", "❌"),
+    WARN(2, "WARN", "⚠️"),
+    INFO(3, "INFO", "ℹ️"),
 
     @OptIn(ExperimentalObjCName::class)
     @ObjCName("_DEBUG")
-    DEBUG(4, "DEBUG"),
-    VERBOSE(5, "VERBOSE");
+    DEBUG(4, "DEBUG", "🐞"),
+    VERBOSE(5, "VERBOSE", "🔍");
 }
